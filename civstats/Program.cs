b10bdeab134172c2746140da7aa73f73991fc706
bayvakoof <bayvakoof@live.com>
2015-10-22 21:37:17 -0400

    Initial commit

---
+++ civstats/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Runtime.Serialization.Json;
+using System.IO;
+
+namespace civstats
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
